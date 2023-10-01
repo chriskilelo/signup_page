1b47deabbfee2a3ea5deee96dfbc9200e5589894
chriskilelo <chriskilelo@gmail.com>
2023-10-01 15:03:56 +0300

    Add the custom made color 'customblue' (#5B86E6) and 'customtruegray' (#949495) to the tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily:{
-        'roboto': ['Roboto', 'sans-serif' ],
-        'exo2': ['"Exo 2"', 'sans-serif' ],
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+        'exo2': ['"Exo 2"', 'sans-serif'],
       },
-      colors: {}
+      colors: {
+        'customblue': {
+          300: '#5B86E6'
+        },
+        'customtruegray': {
+          400: '#949495'
+        }
+      }
     },
   },
   plugins: [],
